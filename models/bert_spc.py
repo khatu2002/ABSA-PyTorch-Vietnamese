9be3cfd102a5713ea012878bb82d8000b22dd711
khatu2002 <115691820+khatu2002@users.noreply.github.com>
2024-09-16 21:13:35 +0700

    Update bert_spc.py

--- models/bert_spc.py
+++ models/bert_spc.py
@@ -10,27 +10,14 @@ class BERT_SPC(nn.Module):
 
     def forward(self, inputs):
         text_bert_indices, bert_segments_ids = inputs[0], inputs[1]
-        
-        # Kiểm tra kiểu dữ liệu đầu vào
-        print("Kiểu của text_bert_indices:", type(text_bert_indices))
-        print("Kiểu của bert_segments_ids:", type(bert_segments_ids))
-        
+
         # Lấy đầu ra từ mô hình BERT
         outputs = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
-        
-        # Kiểm tra kiểu dữ liệu đầu ra từ BERT
-        print("Kiểu của outputs:", type(outputs))
-        
+
         # Thay đổi này để phù hợp với cấu trúc của đầu ra
         pooled_output = outputs.pooler_output  # Dựa vào cấu trúc của outputs
-        
-        # Kiểm tra kiểu dữ liệu đầu ra từ BERT
-        print("Kiểu của pooled_output trước dropout:", type(pooled_output))
-        
+
         pooled_output = self.dropout(pooled_output)
-        
-        # Kiểm tra kiểu dữ liệu đầu ra sau dropout
-        print("Kiểu của pooled_output sau dropout:", type(pooled_output))
         
         logits = self.dense(pooled_output)
         return logits
