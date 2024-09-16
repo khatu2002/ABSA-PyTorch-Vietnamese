fa9ec2053905c10a7ecbca0a17c04a9a77172d3a
khatu2002 <115691820+khatu2002@users.noreply.github.com>
2024-09-16 17:48:57 +0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -102,6 +102,19 @@ class Tokenizer(object):
         if reverse:
             sequence = sequence[::-1]
         return pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
+        
+class Tokenizer4Bert:
+    def __init__(self, max_seq_len, pretrained_bert_name):
+        self.tokenizer = BertTokenizer.from_pretrained(pretrained_bert_name)
+        self.max_seq_len = max_seq_len
+
+    def text_to_sequence(self, text, reverse=False, padding='post', truncating='post'):
+        sequence = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(text))
+        if len(sequence) == 0:
+            sequence = [0]
+        if reverse:
+            sequence = sequence[::-1]
+        return pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
 
 
 class ABSADataset(Dataset):
