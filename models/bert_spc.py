e9ef92b85647f10440f4ff12b19b63a2ec58ac31
khatu2002 <115691820+khatu2002@users.noreply.github.com>
2024-09-16 17:59:02 +0700

    Update bert_spc.py

--- models/bert_spc.py
+++ models/bert_spc.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-# file: BERT_SPC.py
-# author: songyouwei <youwei0314@gmail.com>
-# Copyright (C) 2019. All Rights Reserved.
 import torch
 import torch.nn as nn
-
 
 class BERT_SPC(nn.Module):
     def __init__(self, bert, opt):
@@ -15,7 +10,20 @@ class BERT_SPC(nn.Module):
 
     def forward(self, inputs):
         text_bert_indices, bert_segments_ids = inputs[0], inputs[1]
+        # Kiểm tra kiểu dữ liệu đầu vào
+        print("Kiểu của text_bert_indices:", type(text_bert_indices))
+        print("Kiểu của bert_segments_ids:", type(bert_segments_ids))
+        
+        # Lấy đầu ra từ mô hình BERT
         _, pooled_output = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
+        
+        # Kiểm tra kiểu dữ liệu đầu ra từ BERT
+        print("Kiểu của pooled_output trước dropout:", type(pooled_output))
+        
         pooled_output = self.dropout(pooled_output)
+        
+        # Kiểm tra kiểu dữ liệu đầu ra sau dropout
+        print("Kiểu của pooled_output sau dropout:", type(pooled_output))
+        
         logits = self.dense(pooled_output)
         return logits
